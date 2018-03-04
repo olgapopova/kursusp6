f9791b1d9f9f7fafd80350056c4c060c6528c8db
Olga Popova <Olga.Popova@ivkhk.local>
2018-03-04 13:48:14 +0200

    first 9

--- src/main/java/com/mycompany/p6/App1.java
+++ src/main/java/com/mycompany/p6/App1.java
@@ -6,11 +6,11 @@
 package com.mycompany.p6;
 
 import java.security.SecureRandom;
-import java.util.Base64;
-import java.util.Base64.Encoder;
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
