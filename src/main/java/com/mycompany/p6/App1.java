477334b776d4f391796c92690358c9a2a78792e9
Olga Popova <Olga.Popova@ivkhk.local>
2018-03-04 11:55:20 +0200

    first commit

---
+++ src/main/java/com/mycompany/p6/App1.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.p6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author popova
+ */
+@RestController
+@SpringBootApplication
+
+public class App1 {
+
+    @RequestMapping("/hello")
+    public String helloFunction() {
+        return "HELLO!";
+    }
+
+    @RequestMapping("/hello2")
+    public String helloFunction2(String firstname) {
+        return "HELLO! My name is " + firstname + ".";
+    }
+
+    public static void main(String[] args) {
+        //System.getProperties().put("server.port",4201);
+        SpringApplication.run(App1.class, args);
+    }
+}
